616a4ba9046eae578bfa3daca88e7ba8030207c3
Smolite <shinnara.yuchan@gmail.com>
2025-08-30 17:01:51 +0900

    Update download_file.py

--- smolite/download_file.py
+++ smolite/download_file.py
@@ -1,1 +1,9 @@
+import requests
 
+def download_file(url, save_path):
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
+    with open(save_path, "wb") as f:
+        for chunk in r.iter_content(8192):
+            f.write(chunk)
+    print(f"✅ {save_path} 저장됨")
