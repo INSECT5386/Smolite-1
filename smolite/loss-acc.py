1d3f8e96149c8ccd645f3fdb6e18ed97311bc40e
Smolite <shinnara.yuchan@gmail.com>
2025-08-30 16:45:29 +0900

    Create loss-acc.py

---
+++ smolite/loss-acc.py
@@ -0,0 +1,33 @@
+import tensorflow as ft
+import numpy as np
+
+def smoothed_loss_keras(y_true, y_pred, eps=0.1):
+    y_true = tf.cast(y_true, tf.int32)   # ← 여기 추가
+    mask = tf.cast(tf.not_equal(y_true, pad_id), tf.float32)
+    vocab = tf.shape(y_pred)[-1]
+    y_true_oh = tf.one_hot(y_true, depth=vocab, dtype=tf.float32)
+    y_true_ls = (1.0 - eps) * y_true_oh + eps / tf.cast(vocab, tf.float32)
+    log_probs = tf.nn.log_softmax(y_pred, axis=-1)
+    per_tok = -tf.reduce_sum(y_true_ls * log_probs, axis=-1)
+    per_tok = per_tok * mask
+    return tf.reduce_sum(per_tok) / (tf.reduce_sum(mask)+1e-8)
+
+def masked_accuracy(y_true, y_pred):
+    # y_true를 int로 변환
+    y_true = tf.cast(y_true, tf.int32)
+    mask = tf.cast(tf.not_equal(y_true, pad_id), tf.float32)  # pad 토큰 제외
+    pred_id = tf.argmax(y_pred, axis=-1, output_type=tf.int32)
+    acc = tf.cast(tf.equal(y_true, pred_id), tf.float32) * mask
+    return tf.reduce_sum(acc) / (tf.reduce_sum(mask) + 1e-8)
+
+def masked_perplexity(y_true, y_pred, eps=0.1):
+    y_true = tf.cast(y_true, tf.int32)
+    mask = tf.cast(tf.not_equal(y_true, pad_id), tf.float32)
+    vocab = tf.shape(y_pred)[-1]
+    y_true_oh = tf.one_hot(y_true, depth=vocab, dtype=tf.float32)
+    y_true_ls = (1.0 - eps) * y_true_oh + eps / tf.cast(vocab, tf.float32)
+    log_probs = tf.nn.log_softmax(y_pred, axis=-1)
+    per_tok = -tf.reduce_sum(y_true_ls * log_probs, axis=-1)
+    per_tok = per_tok * mask
+    mean_loss = tf.reduce_sum(per_tok) / (tf.reduce_sum(mask) + 1e-8)
+    return tf.exp(mean_loss)
